2a982d080935d7f9ef8198e428eaee6af1c71b96
Matheus da Costa Ribeiro <matheusgtj@gmail.com>
2022-09-05 19:23:52 -0300

    Adicionando operadores aritimeticos restantes

--- js/script.js
+++ js/script.js
@@ -2,21 +2,31 @@
 
 function somar(){
 //pegando dados a partir do id dos inputs html//
-    let pegar1 = Number(document.getElementById("somajava").value);
-    let pegar2 = Number(document.getElementById("soma2java").value);
+    let pegar1 = parseFloat(document.getElementById("somajava").value);
+    let pegar2 = parseFloat(document.getElementById("soma2java").value);
     //Realizando o cálculo e adicionando-o a div selecionada//
     document.getElementById("resultsoma").innerHTML = "O resultado é: " + (pegar1 + pegar2);
 }
 
 function subtrair() {
     //Pegando dados inseridos nos inputs de maneira resumida criando um array//
-    let pegar3 = [parseInt(document.getElementById('subtracaojava').value), parseInt(document.getElementById('subtracao2java').value)];    
+    let pegar3 = [parseFloat(document.getElementById('subtracaojava').value), parseFloat(document.getElementById('subtracao2java').value)];    
     document.getElementById("resultsubtracao").innerHTML = "O resultado é: " + (pegar3[0] - pegar3[1]);
 }
 
 function divisao() {
-    let pegar4 = [parseInt(document.getElementById("divisaojava").value), parseInt(document.getElementById("divisao2java").value)];
+    let pegar4 = [parseFloat(document.getElementById("divisaojava").value), parseFloat(document.getElementById("divisao2java").value)];
     document.getElementById("resultdivisao").innerHTML ="O resultado é: " + (pegar4[0] / pegar4[1]);
+}
+
+function multiplicacao() {
+    let pegar5 = [parseFloat(document.getElementById("multiplicacaojava").value), parseFloat(document.getElementById("multiplicacao2java").value)];
+    document.getElementById("resultmultiplicacao").innerHTML = "O resultado é: " + (pegar5[0] * pegar5[1]);
+}
+
+function restodivisao() {
+    let pegar6 = [parseFloat(document.getElementById("restodivisaojava").value), parseFloat(document.getElementById("restodivisao2java").value)];
+    document.getElementById("resultrestodivisao").innerHTML = "O resultado é: " + (pegar6[0] % pegar6[1]);
 }
 
 
